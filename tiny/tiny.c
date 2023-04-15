df845748ef0307b987c51b98ecfb8383c2c00521
KINHYEONJI <hyeonji0718@gmail.com>
2023-04-15 13:29:37 +0000

    feat(tiny) : client error를 처리하는 함수 clienterror 함수 구현

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -90,3 +90,27 @@ void doit(int fd) // fd는 client와 연결된 socekt file descriptor
     serve_dynamic(fd, filename, cgiargs); // CGI program(server측 web programming 방식 중 하나)을 실행하여 결과를 생성해 client에게 전송
   }
 }
+
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg) // client와 연결된 socket fd, error의 원인을, 에러코드, 에러의 간랸한 설명, 에러의 자세한 설명
+{
+  char buf[MAXLINE], body[MAXBUF];
+
+  // body에 HTML 형식의 error page 생성
+  sprintf(body, "<html><title>Tiny Error</title>");
+  sprintf(body, "%s<body bgcolor="
+                "ffffff"
+                ">\r\n",
+          body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+  sprintf(body, "%s<hr><em>The Tiny Web Server</em>\r\n", body);
+
+  // buf(문자열)에 HTTP 응답 header 생성
+  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-type: text/html\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+  Rio_writen(fd, buf, strlen(buf));
+  Rio_writen(fd, body, strlen(body));
+}
